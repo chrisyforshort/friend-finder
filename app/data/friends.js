112c0fe5ce859052e41a432b6fd294f844caa750
chrisyshorshort <chritinasarahwu@gmail.com>
2018-04-23 15:42:12 -0700

    home page html/css

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsArray = [
+    {
+        name: 'Finn the Human',
+        photo: '',
+        scores: [3,5,6,7,8,1,2,6,4,6]
+    }
+]
+
+module.exports = friendsArray
--- server.js
+++ server.js
@@ -6,20 +6,29 @@ var path = require('path')
 // new express app
 var app = express()
 
+var PORT = process.env.PORT || 3000
+
 // middleware
 app.use(express.static(path.join(__dirname, 'app')))
+// parse application/x-www-form-urlencoded
 app.use(bodyparser.urlencoded({extended: true}))
+// parse application/json
 app.use(bodyparser.json())
 
 require('./app/routing/htmlRoutes')(app)
 require('./app/routing/apiRoutes')(app)
 
-var PORT = process.env.PORT || 3000
+app.use(function(req, res){
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
 // listening port
 app.listen(PORT, function (e) {
   if (e) throw e
   console.log('http://localhost:'+ PORT);
-  
+
 })
 
 // https://shielded-retreat-99802.herokuapp.com/ | https://git.heroku.com/shielded-retreat-99802.git
