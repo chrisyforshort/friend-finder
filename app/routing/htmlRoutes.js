2e2ac6592dade1ac5be20a5d22ded8f29e45e5c3
chrisyshorshort <chritinasarahwu@gmail.com>
2018-04-24 21:36:54 -0700

    added to surveyhtml and updated paths

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,18 @@
 var path = require('path')
 
 module.exports = function (app) {
+    // Home Page Route //
     app.get('/', function(req, res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
-    })
+    });
     
+    // Survey Route //
     app.get('/survey', function(req, res){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
-    })
+    });
+
+    // Sends to Home Page if any other route entered //
+    app.use(function(req, res){
+        res.sendFile(path.join(__dirname + '../public/home.html'));
+    });
 }
